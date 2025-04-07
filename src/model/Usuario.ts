f8b939e0e863383471b108df354e430dcb5c4809
Juliano da costa <julianodacosta512@hotmail.com>
2025-04-07 15:38:44 -0300

    Criação da model para usuario, findMany, findUnique, create, delete

---
+++ src/model/Usuario.ts
@@ -0,0 +1,65 @@
+import prisma from "@/lib/prisma";
+
+interface AtributosUsuario {
+  id: number;
+  email: string;
+  senha: string;
+  createdAt: Date;
+}
+
+export class Usuario {
+  id: number;
+  email: string;
+  senha: string;
+  createdAt: Date;
+
+  constructor(attributes: AtributosUsuario) {
+    this.id = attributes.id;
+    this.email = attributes.email;
+    this.senha = attributes.senha;
+    this.createdAt = attributes.createdAt;
+  }
+
+  static async findAll() {
+    const usuarios = prisma.usuario.findMany({
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+    return usuarios;
+  }
+
+  static async getById(id: number): Promise<Usuario | null> {
+    const usuario = prisma.usuario.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+    if (!usuario) return null;
+    return usuario;
+  }
+
+  static async create(
+    attributes: Omit<AtributosUsuario, "id" | "createdAt">
+  ): Promise<Usuario> {
+    const { email, senha } = attributes;
+    const newUser = prisma.usuario.create({
+      data: {
+        email,
+        senha,
+      },
+    });
+    return newUser;
+  }
+
+  static async delete(id: number): Promise<Usuario | null> {
+    const usuarioDeletado = prisma.usuario.delete({ where: { id } });
+    if (usuarioDeletado) return null;
+    return usuarioDeletado;
+  }
+}
